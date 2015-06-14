1ef56c3d02e440cea01f546781adcc29fd33d14e
unknown <dominik.guz11@gmail.com>
2015-06-14 10:02:24 +0200

    redis db full entity load test pass & working

--- iotRestfulDatabase/NoSql/RedisNoSqlDataSource.cs
+++ iotRestfulDatabase/NoSql/RedisNoSqlDataSource.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 using Newtonsoft.Json;
+using System.Reflection;
+using StackExchange.Redis.Extensions.Newtonsoft;
+using StackExchange.Redis.Extensions.Core;
 
 namespace iotNoSqlDatabase
 {
@@ -14,10 +17,14 @@ namespace iotNoSqlDatabase
 
         private IDatabase db;
 
+        private NewtonsoftSerializer serializer = new NewtonsoftSerializer();
+        private StackExchangeRedisCacheClient cacheClient;
+
         public RedisNoSqlDataSource()
         {
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
             db = redis.GetDatabase();
+            cacheClient = new StackExchangeRedisCacheClient(serializer);
         }
 
         /********* Serialize/deserialize ********/
@@ -28,11 +35,17 @@ namespace iotNoSqlDatabase
             {
                 if (!property.Name.Equals("ObjectAdress"))
                 {
+                    //is a virtual property - evaluate tree 
+                    if (property.GetMethod.IsVirtual)
+                    {
+
+                    }
+
+                    //
                     if ( property.GetValue(instance) != null)
                     {
                         propertiesInHashEntryList.Add(new HashEntry(property.Name, instance.GetType().GetProperty(property.Name).GetValue(instance).ToString()));
                     }
-                    
                 }
                 else
                 {
@@ -43,14 +56,36 @@ namespace iotNoSqlDatabase
             return propertiesInHashEntryList;
         }
 
-        private static T GetFromEntryList(object instance)
+        private static T GetFromEntryList(List<HashEntry> Entries)
         {
             T parsedEntity = (T)Activator.CreateInstance(typeof(T));
-            foreach (var property in instance.GetType().GetProperties())
+            foreach (var property in parsedEntity.GetType().GetProperties())
             {
-                
+                //try to find matching property in resultset
+                foreach (var entry in Entries)
+                {
+                    if(entry.Name.Equals(property.Name) )
+                    {
+                        try
+                        {
+                            property.SetValue(parsedEntity, Convert.ChangeType(entry.Value, property.PropertyType));
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                       
+                    }
+                }
             }
             return parsedEntity;
+        }
+
+        public string StoreEntityExt(T entity)
+        {
+            string id = Guid.NewGuid().ToString();
+            bool added = cacheClient.Add(id, entity, DateTimeOffset.MaxValue);
+            return id;
         }
 
 
@@ -71,19 +106,25 @@ namespace iotNoSqlDatabase
 
         public virtual T GetById(string id)
         {
+            var resultset = cacheClient.Get<T>(id);
+            return resultset;
+            /*
             var hashes = db.HashGetAll(id);
-            return GetFromEntryList(hashes);
+            return GetFromEntryList(hashes.ToList());
+            */
         }
         
 
         public virtual string Add(T entity)
         {
-            var propertyList = ConvertToHashEntryList(entity);
-            string id = Guid.NewGuid().ToString();
-            db.HashSet(id, propertyList.ToArray());
-            return id;
+            //string id = Guid.NewGuid().ToString();
+            //var propertyList = ConvertToHashEntryList(entity);
+            //db.HashSet(id, propertyList.ToArray());
+            //return id;
 
-           // db.SetAdd(Guid.NewGuid().ToString(), JsonConvert.SerializeObject(entity));
+            // db.SetAdd(Guid.NewGuid().ToString(), JsonConvert.SerializeObject(entity));
+
+            return StoreEntityExt(entity);
         }
 
         public virtual void Update(T entity)
