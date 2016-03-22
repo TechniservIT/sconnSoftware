0787e63d24e385f9b3aa3073144f750f954870b6
DominicusPlatus <dominik.guz11@gmail.com>
2016-03-22 22:58:25 +0100

    viewmodels for alarm config

--- sconnRem/ViewModel/Alarm/AlarmGsmConfigViewModel.cs
+++ sconnRem/ViewModel/Alarm/AlarmGsmConfigViewModel.cs
@@ -4,10 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using sconnConnector.Config;
+using sconnConnector.POCO.Config.Abstract;
 
 namespace sconnRem.ViewModel.Alarm
 {
     public class AlarmGsmConfigViewModel : GsmConfigurationService
     {
+        public AlarmSystemAuthorizedDevicesConfig Config { get; set; }
+
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+        }
+
+        private AuthorizedDevicesConfigurationService _Provider;
+        private AlarmSystemConfigManager _Manager;
+
+        private ICommand _getDataCommand;
+        private ICommand _saveDataCommand;
+
+        private void GetData()
+        {
+        }
+
+        private void SaveData()
+        {
+
+        }
+
+        public AlarmGsmConfigViewModel()
+        {
+            _Name = "Auth";
+        }
     }
 }
--- sconnRem/ViewModel/Alarm/AlarmZoneConfigViewModel.cs
+++ sconnRem/ViewModel/Alarm/AlarmZoneConfigViewModel.cs
@@ -4,12 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using sconnConnector.Config;
+using sconnConnector.POCO.Config.Abstract;
 
 namespace sconnRem.ViewModel.Alarm
 {
     public class AlarmZoneConfigViewModel : ZoneConfigurationService
     {
+        public AlarmSystemAuthorizedDevicesConfig Config { get; set; }
 
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+        }
+
+        private AuthorizedDevicesConfigurationService _Provider;
+        private AlarmSystemConfigManager _Manager;
+
+        private ICommand _getDataCommand;
+        private ICommand _saveDataCommand;
+
+        private void GetData()
+        {
+        }
+
+        private void SaveData()
+        {
+
+        }
+
+        public AlarmZoneConfigViewModel()
+        {
+            _Name = "Auth";
+        }
     }
 
 }
