8e2a96fd0ff671387025c5987773b88c32f1ddcc
dominicusplatus <dominik.guz11@gmail.com>
2016-03-28 22:25:53 +0200

    fix dbo conn cfg , tests

--- iotDatabaseConnector.Tests/ConnectivityUnitTests.cs
+++ iotDatabaseConnector.Tests/ConnectivityUnitTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Data;
+using iotDbConnector.DAL;
 using NUnit.Framework;
 
 namespace iotDatabaseConnector.Tests
@@ -10,43 +12,33 @@ namespace iotDatabaseConnector.Tests
     /// </summary>
     [TestFixture]
     public class ConnectivityUnitTests
-    { 
+    {
+        private iotContext _context;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            this._context = new iotContext();
+        }
 
         public ConnectivityUnitTests()
         {
-            //
-            // TODO: Add constructor logic here
-            //
         }
-
-     
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [Test]
-        public void TestMethod1()
+        public void Test_Database_Connect()
         {
-            
+            _context.Database.Connection.Open();
+            Assert.IsTrue(this._context.Database.Connection.State == ConnectionState.Open);
         }
+
+        [Test]
+        public void Test_Database_Response_Time()
+        {
+            Assert.IsTrue(false);
+        }
+
+
     }
 }
