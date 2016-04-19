4194bdb86afe6743c71f45eedbf79e38557b553d
Dominik Guz <dominik.guz11@gmail.com>
2016-04-19 10:51:41 +0200

    mobile soft runtime, desktop app navigation list items full stretch fix

--- sconnMobile/sconnRem.iOS/SecondViewController.designer.cs
+++ sconnMobile/sconnRem.iOS/SecondViewController.designer.cs
@@ -1,17 +1,21 @@
-﻿//
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace sconnRem.iOS
 {
-    [Register("SecondViewController")]
-    partial class SecondViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("SecondViewController")]
+	partial class SecondViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
 }
