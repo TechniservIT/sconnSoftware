95f53733c583fa7ee2499ebb28d7f2d3e5ee3d48
Dominik Guz <dominik.guz11@gmail.com>
2016-05-24 11:40:28 +0200

    mobie list view

---
+++ sconnMobile/sconnMobileForms/sconnMobileForms/View/SiteManagment/SiteListView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sconnConnector.POCO.Config;
+using SiteManagmentService;
+using Xamarin.Forms;
+
+namespace sconnMobileForms.View.SiteManagment
+{
+    public partial class SiteListView : ContentPage
+    {
+
+        public ISiteRepository Repository { get; set; }
+        public List<sconnSite> Sites { get; set; }
+
+        public ListView List { get; set; }
+
+        private void LoadList()
+        {
+            Sites = Repository.GetAll().ToList();
+        }
+
+        public SiteListView()
+        {
+
+            Repository = new SiteSqlRepository();
+            Sites = new List<sconnSite>();
+            List = new ListView();
+            List.ItemsSource = Sites;
+
+            InitializeComponent();
+
+        }
+    }
+}
