6ad0e66de737b9f810bc279a03ea0fd6688d0092
DominicusPlatus <dominik.guz11@gmail.com>
2016-05-25 23:33:38 +0200

    create projects for jtag/obd software, portable

--- sconnMobile/Shared/sconnRemMobile/sconnRemMobile.Droid/Resources/Resource.designer.cs
+++ sconnMobile/Shared/sconnRemMobile/sconnRemMobile.Droid/Resources/Resource.designer.cs
@@ -26,8 +26,6 @@ namespace sconnRemMobile.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::sconnRemMobile.Droid.Resource.String.ApplicationName;
-			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::sconnRemMobile.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -95,16 +93,10 @@ namespace sconnRemMobile.Droid
 		{
 			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int app_name = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
-			
-			// aapt resource value: 0x7f040003
-			public const int app_name = 2130968579;
-			
-			// aapt resource value: 0x7f040002
-			public const int hello = 2130968578;
+			public const int hello = 2130968576;
 			
 			static String()
 			{
